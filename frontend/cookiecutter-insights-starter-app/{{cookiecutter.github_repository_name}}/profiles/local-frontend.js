7c751eb2d4900bac284269b9b9a2a2a4e76f4848
Ryan Long <rlong@redhat.com>
2019-12-16 11:39:09 -0500

    remove submodule

---
+++ frontend/cookiecutter-insights-starter-app/{{cookiecutter.github_repository_name}}/profiles/local-frontend.js
@@ -0,0 +1,13 @@
+/*global module*/
+
+const SECTION = '{{cookiecutter.insights_platform_bundle}}';
+const APP_ID = '{{cookiecutter.insights_platform_app_name}}';
+const FRONTEND_PORT = 8002;
+const routes = {};
+
+routes[`/beta/${SECTION}/${APP_ID}`] = { host: `https://localhost:${FRONTEND_PORT}` };
+routes[`/${SECTION}/${APP_ID}`]      = { host: `https://localhost:${FRONTEND_PORT}` };
+routes[`/beta/apps/${APP_ID}`]       = { host: `https://localhost:${FRONTEND_PORT}` };
+routes[`/apps/${APP_ID}`]            = { host: `https://localhost:${FRONTEND_PORT}` };
+
+module.exports = { routes };
